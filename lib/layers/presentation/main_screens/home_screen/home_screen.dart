c89b8098678f21c46ebd26c98cda0392b47b5884
karthik324 <karthikdileep003@gmail.com>
2023-11-02 23:56:04 +0530

    fetched audio from local

--- lib/layers/presentation/main_screens/home_screen/home_screen.dart
+++ lib/layers/presentation/main_screens/home_screen/home_screen.dart
@@ -10,9 +10,35 @@ import 'package:mello_d/layers/styles/app_styles.dart';
 import 'package:mello_d/layers/widgets/custom_app_bar.dart';
 import 'package:mello_d/layers/widgets/divider_horizontal.dart';
 import 'package:mello_d/layers/widgets/music_tile.dart';
+import 'package:on_audio_query/on_audio_query.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final OnAudioQuery _audioQuery = OnAudioQuery();
+
+  bool _hasPermission = false;
+
+  @override
+  void initState() {
+    super.initState();
+    checkAndRequestPermissions();
+  }
+
+  checkAndRequestPermissions({bool retry = false}) async {
+    // The param 'retryRequest' is false, by default.
+    _hasPermission = await _audioQuery.checkAndRequest(
+      retryRequest: retry,
+    );
+
+    // Only call update the UI if application has all required permissions.
+    _hasPermission ? setState(() {}) : null;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -50,6 +76,48 @@ class HomeScreen extends StatelessWidget {
                 padding: EdgeInsets.symmetric(horizontal: size.width * 0.015),
                 child: const RecentlyPlayedView(),
               ),
+              !_hasPermission
+                  ? noAccessToLibraryWidget()
+                  : FutureBuilder<List<SongModel>>(
+                      // Default values:
+                      future: _audioQuery.querySongs(
+                        sortType: null,
+                        orderType: OrderType.ASC_OR_SMALLER,
+                        uriType: UriType.EXTERNAL,
+                        ignoreCase: true,
+                      ),
+                      builder: (context, item) {
+                        // Display error, if any.
+                        if (item.hasError) {
+                          return Text(item.error.toString());
+                        }
+
+                        // Waiting content.
+                        if (item.data == null) {
+                          return const CircularProgressIndicator();
+                        }
+
+                        // 'Library' is empty.
+                        if (item.data!.isEmpty) return const Text("Nothing found!");
+
+                        // You can use [item.data!] direct or you can create a:
+                        // List<SongModel> songs = item.data!;
+                        return ListView.builder(
+                          physics: const NeverScrollableScrollPhysics(),
+                          shrinkWrap: true,
+                          itemCount: item.data!.length,
+                          itemBuilder: (context, index) {
+                            return MusicTile(
+                              artist: item.data?[index].artist ?? 'Unknown',
+                              controller: _audioQuery,
+                              heroTag: 'Hero',
+                              title: item.data?[index].title ?? 'Unknown',
+                              id: item.data?[index].id ?? 0,
+                            );
+                          },
+                        );
+                      },
+                    ),
               2.height(context),
               const LightGreyDivisionHori(),
               Padding(
@@ -57,6 +125,27 @@ class HomeScreen extends StatelessWidget {
                 child: MusicalSelectionView(size: size),
               )
             ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget noAccessToLibraryWidget() {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: Colors.redAccent.withOpacity(0.5),
+      ),
+      padding: const EdgeInsets.all(20),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const Text("Application doesn't have access to the library"),
+          const SizedBox(height: 10),
+          ElevatedButton(
+            onPressed: () => checkAndRequestPermissions(retry: true),
+            child: const Text("Allow"),
           ),
         ],
       ),
@@ -167,37 +256,37 @@ class RecentlyPlayedView extends StatelessWidget {
         1.height(context),
         Column(
           children: [
-            InkWell(
-              borderRadius: BorderRadius.circular(AppStyles.defaultBorderRadius),
-              onTap: () {
-                AppRoutes.goTo(
-                  context: context,
-                  fromBottomToUp: true,
-                  screen: PlayMusicScreen(),
-                );
-              },
-              child: const Padding(
-                padding: EdgeInsets.all(4),
-                child: MusicTile(
-                  heroTag: 'hero',
-                  artist: 'Travis Scott',
-                  image: AppImages.travisScottPlaceHolder,
-                  title: 'Highest in the room',
-                ),
-              ),
-            ),
-            const MusicTile(
-              heroTag: 'hah',
-              artist: 'Home',
-              image: AppImages.resonancePlaceHolder,
-              title: 'Resonance',
-            ),
-            const MusicTile(
-              heroTag: 'asdasdf',
-              artist: 'Strawberry Guy',
-              image: AppImages.strawBerryGuyPlaceHolder,
-              title: 'What would I do',
-            ),
+            // InkWell(
+            //   borderRadius: BorderRadius.circular(AppStyles.defaultBorderRadius),
+            //   onTap: () {
+            //     AppRoutes.goTo(
+            //       context: context,
+            //       fromBottomToUp: true,
+            //       screen: const PlayMusicScreen(),
+            //     );
+            //   },
+            //   // child: const Padding(
+            //   //   padding: EdgeInsets.all(4),
+            //   //   child: MusicTile(
+            //   //     heroTag: 'hero',
+            //   //     artist: 'Travis Scott',
+            //   //     image: AppImages.travisScottPlaceHolder,
+            //   //     title: 'Highest in the room',
+            //   //   ),
+            //   // ),
+            // ),
+            // const MusicTile(
+            //   heroTag: 'hah',
+            //   artist: 'Home',
+            //   image: AppImages.resonancePlaceHolder,
+            //   title: 'Resonance',
+            // ),
+            // const MusicTile(
+            //   heroTag: 'asdasdf',
+            //   artist: 'Strawberry Guy',
+            //   image: AppImages.strawBerryGuyPlaceHolder,
+            //   title: 'What would I do',
+            // ),
           ],
         ),
       ],
--- lib/layers/widgets/music_tile.dart
+++ lib/layers/widgets/music_tile.dart
@@ -1,25 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:mello_d/extensions/int_extension.dart';
 import 'package:mello_d/layers/styles/app_styles.dart';
+import 'package:on_audio_query/on_audio_query.dart';
 
 class MusicTile extends StatelessWidget {
   const MusicTile({
     super.key,
     required this.artist,
-    required this.image,
     required this.title,
     required this.heroTag,
+    required this.controller,
+    required this.id,
   });
 
   final String title;
   final String artist;
-  final String image;
   final String heroTag;
+  final int id;
+  final OnAudioQuery controller;
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 5),
+      padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.center,
@@ -27,18 +31,14 @@ class MusicTile extends StatelessWidget {
           Row(
             children: [
               Hero(
-                tag: heroTag ,
-                child: Container(
+                tag: heroTag,
+                child: SizedBox(
                   height: 60,
                   width: 60,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    image: DecorationImage(
-                      fit: BoxFit.cover,
-                      image: AssetImage(
-                        image,
-                      ),
-                    ),
+                  child: QueryArtworkWidget(
+                    controller: controller,
+                    id: id,
+                    type: ArtworkType.AUDIO,
                   ),
                 ),
               ),
@@ -46,7 +46,13 @@ class MusicTile extends StatelessWidget {
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(title, style: AppStyles.semiBold),
+                  SizedBox(
+                    width: size.width - 125,
+                    child: Text(
+                      title,
+                      style: AppStyles.semiBold,
+                    ),
+                  ),
                   Text(artist, style: AppStyles.medium),
                 ],
               ),
